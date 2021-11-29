755bb60803b70e9af58b2f7e26b19e4e47821612
Devin <support@adeept.com>
2021-11-29 16:51:19 +0800

    first commit

---
+++ adeept_picarpro/server/apps.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response, send_from_directory
+from flask_cors import *
+# import camera driver
+
+from camera_opencv import Camera
+import threading
+
+# Raspberry Pi camera module (requires picamera package)
+# from camera_pi import Camera
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+camera = Camera()
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+class webapp:
+    def __init__(self):
+        self.camera = camera
+
+    def modeselect(self, modeInput):
+        Camera.modeSelect = modeInput
+
+    def colorFindSet(self, H, S, V):
+        camera.colorFindSet(H, S, V)
+
+    def thread(self):
+        app.run(host='0.0.0.0', port=10123,threaded=True)
+
+    def startthread(self):
+        fps_threading=threading.Thread(target=self.thread)         #Define a thread for FPV and OpenCV
+        fps_threading.setDaemon(False)                             #'True' means it is a front thread,it would close when the mainloop() closes
+        fps_threading.start()                                     #Thread starts
+
+
